7d3489e3e3cac50124859fd2f38d2fbc292af9a9
Kevin Reuning <kevin.reuning@gmail.com>
2020-07-14 16:03:22 -0400

    added images

--- tweet_bot.py
+++ tweet_bot.py
@@ -11,6 +11,10 @@ import textwrap
 from TwitterAPI import TwitterAPI
 import config 
 import time
+from io import BytesIO
+
+from PIL import Image, ImageDraw, ImageFont
+
 
 
 api = TwitterAPI(config.key,  
@@ -19,10 +23,12 @@ api = TwitterAPI(config.key,
                 config.access_secret)
 
 
-wrapper = textwrap.TextWrapper(width=255)
+wrapper = textwrap.TextWrapper(width=50)
+byte_io = BytesIO()
 
 df = pd.read_csv("tweets.csv")
 df_past = pd.read_csv("tweeted.csv")
+
 
 while True:
     tweet_row = df.sample(1)
@@ -33,25 +39,73 @@ while True:
         break
         
 
-message = wrapper.wrap(tweet_row.Tweet.iloc[0])
 
-if len(message) > 1:
-    for ii in range(len(message[:-1])):
-        message[ii] = (message[ii] + "..." + str(ii+1) +  "/" 
-                       + str(len(message)))
+message = tweet_row.Tweet.iloc[0]
+
+if(len(message) > 250):
+    message = "\n".join(wrapper.wrap(tweet_row.Tweet.iloc[0]))
+    width = 1200
+    height = 675
+    img = Image.new('RGB', (width, height), color=(40, 60, 140))
+    draw = ImageDraw.Draw(img)
+    fontsize = 1  # starting font size
+    img_fraction = .9
+    font = ImageFont.truetype("LibreBaskerville-Bold.ttf", fontsize)
     
-
-tweet_id = 0
-for tweet in message:
-    if tweet_id ==0:
-        r = api.request('statuses/update', {'status': tweet})
-    else:
-        tweet = "@workingwcovid " + tweet
-        r = api.request('statuses/update', {'status': tweet, 
-                                            'in_reply_to_status_id':tweet_id})
+    
+    while True:
+        check_size = draw.multiline_textsize(message,   font=font)
+        if check_size[0] > img_fraction*width or check_size[1] > img_fraction*height:
+            break 
+        else:
+            fontsize += 1
+            font = ImageFont.truetype("LibreBaskerville-Bold.ttf", 
+                                      fontsize)
+    
+    
+    text_size = draw.multiline_textsize(message,  font=font)
+    draw.multiline_text(xy=((width - text_size[0])/2, 
+                            (height - text_size[1])/2), text=message,  
+                        align='center',font=font,   fill='white')
+    
+    font = ImageFont.truetype("LibreBaskerville-Bold.ttf", 
+                              15)
+    size = draw.textsize("Complaint Reported to OSHA\nSee: @WorkingWCovid", 
+                         font=font)
+    draw.multiline_text(xy=(width-size[0] - 5, 
+                            height-size[1] - 5), 
+                        text="Complaint Reported to OSHA\nSee: @WorkingWCovid",
+                        align='right', font=font, 
+                        fill='white')
+    
+    img.save("tmp.PNG",
+             dpi=(300,300), optimize=True, 
+             compress_level=4) # save it
+    img.save(byte_io, "PNG",
+             dpi=(300,300), optimize=True, 
+             compress_level=4) # save it
+    
+    data = byte_io.getvalue()
+    r = api.request('media/upload', None, {'media': data})
+    
+    status = tweet_row.Tweet.iloc[0]
+    status = textwrap.shorten(status, width=200, placeholder='...')
+    
+    if r.status_code == 200:
+        media_id = r.json()['media_id']
         
-    tweet_id = r.json()['id']
-    time.sleep(1)
+        # r = api.request('media/metadata/create', 
+        #                 {'media_id':media_id,
+        #                  "alt_text": {
+        #                      "text": caption[0:999]} })
+        r = api.request('statuses/update', 
+                        {'status': status, 'media_ids': media_id})
+else:
+
+    r = api.request('statuses/update', 
+                {'status': message})
+        
+
 
 
 
